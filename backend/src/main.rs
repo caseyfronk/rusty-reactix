b5d8eaa0f56b7d99ddda103d2c8331ec6e61c311
Casey Fronk <caseyjfronk@gmail.com>
2025-01-06 18:42:55 -0700

    Set up basic Rust (Actix) application that serves a React/Vite application.

---
+++ backend/src/main.rs
@@ -0,0 +1,42 @@
+use actix_files;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = env::var("HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port: u16 = env::var("PORT")
+        .unwrap_or_else(|_| "8080".to_string())
+        .parse()
+        .expect("PORT must be a valid u16");
+
+    println!("🚀 Starting server at http://{}:{}", host, port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .service(actix_files::Files::new("/", "./static").index_file("index.html"))
+    })
+    .bind((host, port))?
+    .run()
+    .await
+}
+
+#[get("/health")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Health check OK")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
